71b2e908f6b9c5559463389ffca6f79cf57b793d
”Igor <igor.yarik@gmail.com>
2022-11-24 17:00:57 +0200

    added data City

--- scripts.js
+++ scripts.js
@@ -1,19 +1,32 @@
 const userYearOfBirth = prompt('запитати у користувача рік народження;')
 const userCity = prompt('запитати в нього, в якому місті він живе;')
 const userSport = prompt('запитати його улюблений вид спорту.')
-console.log(userYearOfBirth, userCity, userSport)
 
 let date = new Date
 const userAge = (date.getFullYear() - userYearOfBirth)
-alert(`Ваш вік ${userAge}`)
 
+/* const capitalUkraine = ('Київ')
+const capitalUSA = ('Вашингтон')
+const capitalGB = ('Лондон') */
 
-const capitalUkraine = ('Київ', 'Kyiv', 'Киев')
-
-if (userCity == capitalUkraine) {
-    alert(`Ти живеш у столиці ${userCity}`)
-} else { 
-    alert(`ти живеш у місті ${userCity}`)
+if (userCity === 'Київ' || userCity === 'Kyiv' || userCity === 'Киев') {
+    console.log(`Ваш вік ${userAge} років; Ти живеш у столиці України`)
+} else if (userCity === 'Вашингтон' || userCity === 'Washington') {
+    console.log(`Ваш вік ${userAge} років; Ти живеш у столиці Сполучених Штатів Америки`)
+} else if (userCity === 'Лондон' || userCity === 'London') {
+    console.log(`Ваш вік ${userAge} років; Ти живеш у столиці Великобританії`)
+} else if (userCity === null) { 
+    console.log('Ну ты и Вася')
+} else {
+    console.log(`Ваш вік ${userAge} років; ти живеш у місті ${userCity}`)
 }
 
-
+if (userSport === 'Футбол') {
+    console.log('Круто! Хочеш стати Андрієм Шевченко?')
+} else if (userSport === 'Хоккей') {
+    console.log('Круто! Хочеш стати Андрієм Шевченко?')
+} else if (userSport === 'Баскетбол') {
+    console.log('Круто! Хочеш стати Андрієм Шевченко?')
+} else if (userSport === null) {
+    console.log('Как можно не увлекаться спортом???')
+}
