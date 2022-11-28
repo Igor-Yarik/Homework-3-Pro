48b1716cbc7353d50ae6ee0c2db21b8039913cfe
”Igor <igor.yarik@gmail.com>
2022-11-28 17:15:43 +0200

    final

--- scripts.js
+++ scripts.js
@@ -1,41 +1,44 @@
-/* const userYearOfBirth = prompt('запитати у користувача рік народження;') */
-const userCity = prompt('В якому місті ви проживаєте;')
-const userSport = prompt('запитати його улюблений вид спорту.')
+const userYearOfBirth = prompt('Введіть будь ласка свій рік народження?', '');
+console.log(userYearOfBirth);
+let date = new Date
+let userAge = (date.getFullYear() - userYearOfBirth)
 
-let Soccer = 'Футбол'
-let Hockey = 'Хоккей'
-let Basketball = 'Баскетбол'
+if (userYearOfBirth === null) {
+    alert(`Шкода, що Ви не захотіли ввести свій рік народження.`);
+} else if (userYearOfBirth.length === 0) {
+    userAge = prompt('Ви не ввели свій рік народження, спробуйте ще раз.');
+} else if (userYearOfBirth) {
+    alert(`Ваш вік ${userAge} років.`)
+}
 
-let answerSoccer = 'Ви хочите стати футболістом'
-let answerHockey = 'Ви хочите стати хокеїстом'
-let answerBasketball = 'Ви хочите стати баскетболістом'
-
-/* let date = new Date
-const userAge = (date.getFullYear() - userYearOfBirth) */
-
-
-if (userCity === 'Київ' || userCity === 'Киев' || userCity === 'Kyiv') {
-    alert(`Ви живете у столиці України;`)
-} else if (userCity === 'Лондон' || userCity === 'London') { 
-    alert(`Ви живете у столиці Великобританії;`)
-} else if (userCity === 'Вашингтон' || userCity === 'Washington') { 
-    alert('Ви живете у столиці Сполучених Штатів Америки')
+const userCity = prompt('Введіть будь ласка ваше місто проживання.', '')
+console.log(userCity);
+if (userCity === 'Київ' || userCity === 'Kyiv' || userCity === 'Киев') {
+    alert(`Ви живете у столиці України`)
+} else if (userCity === 'Вашингтон' || userCity === 'Washington') {
+    alert(`Ви живете у столиці Сполучених Штатів Америки`)
+} else if (userCity === 'Лондон' || userCity === 'London') {
+    alert(`Ви живете у столиці Великобританії`)
 } else if (userCity === null) { 
-    alert('Шкода, що Ви не захотіли ввести своє місце проживання')
-} else {
+    alert(`Шкода, що Ви не захотіли ввести своє місто проживання.`)
+} else if (!isNaN(userCity)) {
+    userCity = prompt('Введіть корректно назву міста з великої літери.', '');
+} else if (userCity) {
     alert(`Ви живете у місті ${userCity}`)
 }
 
-
-    
-if (userSport === Soccer) {
-    console.log(` ${answerSoccer}`)
-} else if (userSport === Hockey) { 
-    console.log(`${answerHockey}`)
-} else if (userSport === Basketball) { 
-    console.log(`${answerBasketball}`)
-} else if (userCity === null) { 
-    console.log('Шкода, що Ви не захотіли ввести свій улюблений вид спорту')
+const userSport = prompt('Який ваш улюблений вид спорту?', '');
+console.log(userSport);
+if (userSport === null) {
+    alert(`Шкода, що Ви не захотіли вказати ваш улюблений вид спорту`);
+} else if (!isNaN(userSport)) {
+    userSport = prompt('Введіть корректно назву спорту з великої літери.', '');
+} else if (userSport === 'Футбол'){
+       alert(`Круто! Хочете стати футболістом?`);
+} else if(userSport === 'Хоккей') {
+    alert(`Круто! Хочете стати хоккеистом?`);
+} else if (userSport === 'Баскетбол' ) {
+    alert(`Круто! Хочете стати басетболистом?`);
 } else {
-    console.log(`${userSport}, це круто! Вперед до чемпіонства!`)
+    alert(`${userSport}, це круто! Вперед до чемпіонства!`)
 }
